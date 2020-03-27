cfc41291d3244af966eb31d8c8a521b3b43faeaf
infernezor@gmail.com <infernezor@gmail.com>
2020-03-27 14:20:50 -0400

    fixed small error with return statement. Thanks, Sean.

--- src/component/userProfileList.jsx
+++ src/component/userProfileList.jsx
@@ -1,12 +1,21 @@
-import React, { Component } from "react";
+import React from "react";
 import UserProfile from "./userProfileList";
 
 const UserProfileList = props => {
   const { userData } = props; //grab data from app.js
-
-  return {
+  return (
     //need a map function.
-  };
+    <h1>
+      failure!
+      {userData.length > 0 ? (
+        userData.map(users => {
+          return <h1>Ya filthy demon!</h1>;
+        })
+      ) : (
+        <p> You got jack in that ternary, bud.</p>
+      )}
+    </h1>
+  );
 };
 
 export default UserProfileList;
